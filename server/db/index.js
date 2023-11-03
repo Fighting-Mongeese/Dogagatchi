a5ea29b2191ed66d606c0fffb440249f76de552d
jshep23prog <jshep23@gmail.com>
2023-11-03 16:53:14 -0500

    "add/ successfully connected cloud database and initialized schema"

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,41 @@
 // 0. Index is the Entry point
 // require mongoose after running npm install mongodb
-const { ATLAS_PASSWORD } = process.env;
 const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const mongoUri = `mongodb+srv://dogagotchi:${ATLAS_PASSWORD}@dogtrivia.gxhy4jj.mongodb.net/?retryWrites=true&w=majority`;
+// const { ATLAS_PASSWORD } = process.env;
 
-const db = mongoose.connect(mongoUri)
-  .then(() => {
-    console.log('Connection to DB successful');
+const uri = 'mongodb+srv://dogagotchi:5M0u9BvJUfwNcQZZ@dogtrivia.gxhy4jj.mongodb.net/dogtrivia';
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch((err) => {
-    console.error('Connection to DB failed', err);
-  });
+  .then(() => console.log('Database connected!'))
+  .catch((err) => console.log(err));
+
+// setup schema(s)
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String, // may be changed with passport implementation
+  coins: Number,
+  breeds: [String],
+});
+// creates user docs in the db
+const User = mongoose.model('User', userSchema);
+// schema for Dogs
+const dogSchema = new mongoose.Schema({
+  name: String,
+  img: String,
+  isHungry: Boolean,
+  isHappy: Boolean,
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+});
+
+const Dog = mongoose.model('Dog', dogSchema);
+
+module.exports = {
+  User,
+  Dog,
+};
--- server/index.js
+++ server/index.js
@@ -3,11 +3,24 @@ const path = require('path');
 
 const app = express();
 const port = 4000;
+const { User } = require('./db/index');
 
 const distPath = path.resolve(__dirname, '..', 'dist');
 
 app.use(express.static(distPath)); // Statically serve up client directory
-
+const testFunc = () => {
+  User.create({
+    username: 'James',
+    coins: 8,
+  })
+    .then((newUser) => {
+      console.log('Successful add', newUser);
+    })
+    .catch((err) => {
+      console.error('Failed to add user', err);
+    });
+};
+//testFunc();
 app.listen(port, () => {
   console.log(`
   Listening at: http://127.0.0.1:${port}

deb9ef300508004d77323c14ce189546ab912c43
evmaperry <evmaperry@gmail.com>
2023-11-04 13:08:37 -0400

    CLIENT / add answer submission handling in quiz component; db updates from front end

--- client/components/Quiz.jsx
+++ client/components/Quiz.jsx
@@ -8,7 +8,9 @@ function Quiz(props) {
   const [dogs, setDogs] = useState([]); // 4 urls of dog images loaded from API
   const [solutionUrl, setSolutionUrl] = useState(''); // default to zero, get set in set state; maybe math 
   const [userId, setUserId] = useState('');
-  // const [coins, setCoins] = useState(0); // user coins loaded from db
+  // const [userCoins, setCoins] = useState(0); // user coins loaded from db
+  const [alertText, setAlertText] = useState('Start earning coins by correctly selecting the breed pictured!');
+
 
   const getDogs = () => new Promise((resolve, reject) => {
     axios.get('/getDogs')
@@ -24,7 +26,6 @@ function Quiz(props) {
   });
 
   const setSolutionDog = (dogArray) => {
-    console.log('inside sSD');
     const randomIndex = Math.floor(Math.random() * dogs.length);
     setSolutionUrl(dogArray[randomIndex]);
   };
@@ -32,10 +33,33 @@ function Quiz(props) {
   const handleAnswerSubmission = (e) => {
     const { value } = e.target; // unpack event
 
+    // value is a url assigned to the button's value
     if (value === solutionUrl) {
-      console.log('Correct! You gained coins!', userId)
+      axios.put(`/correctAnswerUpdate/${userId}`, {
+        dog: {
+          url: solutionUrl,
+        },
+      })
+        .then((user) => {
+          console.log(user);
+          setAlertText(`Correct! Keep up the good work! You now have ${user.data.coinCount} coins`);
+          getDogs()
+            .then((dogArray) => {
+              setSolutionDog(dogArray);
+            });
+        })
+        .catch((err) => {
+          console.error('CLIENT ERROR: failed to start new round after correct answer', err);
+        });
     } else {
-      alert('Wrong answer! You gained no points!', userId)
+      setAlertText('Nice try! Have another go!');
+      getDogs()
+        .then((dogArray) => {
+          setSolutionDog(dogArray);
+        })
+        .catch((err) => {
+          console.error('CLIENT ERROR: failed to start new round after correct answer', err);
+        });
     }
   };
 
@@ -56,17 +80,25 @@ function Quiz(props) {
   });
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
 
       <h3>Add your user ID in the input below</h3>
-      <input type='text' onChange={(e) => setUserId(e.target.value)} />
+      <p>654660d5a2683bbdcb573a83, 6546609aa2683bbdcb573a82, 65466062a2683bbdcb573a81</p>
+      <input type="text" onChange={(e) => setUserId(e.target.value)} value={userId} />
       <h3>Oh Gawd, what is that thing?</h3>
       <Image style={{ maxHeight: '500px', maxWidth: '500px' }} src={solutionUrl} rounded />
-      <h2>Select the correct dog breed to win coins</h2>
+      <h2>{alertText}</h2>
       <div style={{ display: 'flex' }}>
         {dogButtons}
       </div>
+      <div>
 
+      </div>
     </div>
   );
 }

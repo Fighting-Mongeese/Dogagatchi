a98857d6b22231ce280fa53340fb029a31ec73d0
evmaperry <evmaperry@gmail.com>
2023-11-04 10:41:05 -0400

    Client / add css file and react-bootstrap

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
+import '../app.css';
 
 function App() {
   return (
     <div>
       <div>APP RENDERING</div>
-
+      <Button>Bootstrap Button</Button>
     </div>
   );
 }
--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import 'bootstrap/dist/css/bootstrap.min.css'; // imports bootstrap library
 import App from './components/App.jsx';
 
 const container = document.getElementById('app');

f90e8f907584ebd9d7c35eadb132043df50254aa
evmaperry <evmaperry@gmail.com>
2023-11-06 11:04:04 -0500

    adding duplicates validation

--- client/components/Quiz.jsx
+++ client/components/Quiz.jsx
@@ -8,13 +8,87 @@ function Quiz(props) {
   const [dogs, setDogs] = useState([]); // 4 urls of dog images loaded from API
   const [solutionUrl, setSolutionUrl] = useState(''); // default to zero, get set in set state; maybe math 
   const [userId, setUserId] = useState('');
+  const [alertText, setAlertText] = useState('Start earning coins by correctly selecting the breed pictured!');
+
+  const parseUrl = (url) => {
+    const noDomain = url.slice(url.indexOf('breeds/') + 7);
+    let breed = noDomain.slice(0, noDomain.indexOf('/'));
+    if (breed.includes('-')) {
+      breed = breed.split('-').reverse().join(' ');
+    }
+    return breed;
+  };
+
+  const checkForDuplicateDogs = (urlArray) => {
+    let hasDuplicates = false;
+
+    const breedArray = urlArray.map((url) => parseUrl(url));
+
+    for (let i = 0; i < breedArray.length; i += 1) {
+      const target = JSON.parse(JSON.stringify(breedArray));
+      const value = target[i];
+      target.splice(i, 1);
+      if (target.includes(value)) {
+        hasDuplicates = true;
+      }
+    }
+
+    console.log(breedArray, 'has duplicates?', hasDuplicates);
+    return hasDuplicates;
+  };
+
+/* *****************************************************
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+
+function Quiz(props) {
+  const [dogs, setDogs] = useState([]); // 4 urls of dog images loaded from API
+  const [solutionUrl, setSolutionUrl] = useState(''); // default to zero, get set in set state; maybe math 
+  const [userId, setUserId] = useState('');
   // const [userCoins, setCoins] = useState(0); // user coins loaded from db
   const [alertText, setAlertText] = useState('Start earning coins by correctly selecting the breed pictured!');
+
+  const parseUrl = (url) => {
+    const noDomain = url.slice(url.indexOf('breeds/') + 7);
+    let breed = noDomain.slice(0, noDomain.indexOf('/'));
+    if (breed.includes('-')) {
+      breed = breed.split('-').reverse().join(' ');
+    }
+    return breed;
+  };
+
+  const checkForDuplicateDogs = (urlArray) => {
+    let hasDuplicates = false;
+
+    const breedArray = urlArray.map((url) => parseUrl(url));
+
+    for (let i = 0; i < breedArray.length; i += 1) {
+      const target = JSON.parse(JSON.stringify(breedArray));
+      const value = target[i];
+      target.splice(i, 1);
+      if (target.includes(value)) {
+        hasDuplicates = true;
+      }
+    }
+
+    console.log(breedArray, 'has duplicates?', hasDuplicates);
+    return hasDuplicates;
+  };
 
   const getDogs = () => new Promise((resolve, reject) => {
     axios.get('/getDogs')
       .then((dogArray) => {
-        //console.log(dogArray.data);
+        // console.log(dogArray.data);
+        
+        // if there are duplicates
+        if (checkForDuplicateDogs(dogArray.data)) {
+          // run getDogs again (until no duplicates)
+          getDogs();
+        }
         setDogs(dogArray.data);
         resolve(dogArray.data);
       })
@@ -73,6 +147,110 @@ function Quiz(props) {
     if (breed.includes('-')) {
       breed = breed.split('-').reverse().join(' ');
     }
+    return (
+      <Button value={url} style={{ margin: '20px' }} key={index} onClick={handleAnswerSubmission} type="button">{breed}</Button>
+    );
+  });
+
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+
+      <h3>Add your user ID in the input below</h3>
+      <p>654660d5a2683bbdcb573a83, 6546609aa2683bbdcb573a82, 65466062a2683bbdcb573a81</p>
+      <input type="text" onChange={(e) => setUserId(e.target.value)} value={userId} />
+      <h3>Oh Gawd, what is that thing?</h3>
+      <Image style={{ maxHeight: '500px', maxWidth: '500px' }} src={solutionUrl} rounded />
+      <h2>{alertText}</h2>
+      <div style={{ display: 'flex' }}>
+        {dogButtons}
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Quiz;
+
+
+
+*/
+
+  const getDogs = () => new Promise((resolve, reject) => {
+    axios.get('/getDogs')
+      .then((dogArray) => {
+        // console.log(dogArray.data);
+
+        // if there are duplicates
+        if (checkForDuplicateDogs(dogArray.data)) {
+          // run getDogs again (until no duplicates)
+          getDogs();
+        }
+        setDogs(dogArray.data);
+        resolve(dogArray.data);
+      })
+      .catch((err) => {
+        console.error('CLIENT ERROR: failed to GET dogs', err);
+        reject(err);
+      });
+  });
+
+  const setSolutionDog = (dogArray) => {
+    const randomIndex = Math.floor(Math.random() * dogs.length);
+    setSolutionUrl(dogArray[randomIndex]);
+  };
+
+  const handleAnswerSubmission = (e) => {
+    const { value } = e.target; // unpack event
+
+    // value is a url assigned to the button's value
+    if (value === solutionUrl) {
+      axios.put(`/correctAnswerUpdate/${userId}`, {
+        dog: {
+          url: solutionUrl,
+        },
+      })
+        .then((user) => { // put request returns updated user object
+          console.log(user);
+          setAlertText(`Correct! Keep up the good work! You now have ${user.data.coinCount} coins`);
+          getDogs()
+            .then((dogArray) => {
+              setSolutionDog(dogArray);
+            });
+        })
+        .catch((err) => {
+          console.error('CLIENT ERROR: failed to start new round after correct answer', err);
+        });
+    } else { // if the answer is wrong
+      setAlertText('Nice try! Have another go!');
+      getDogs()
+        .then((dogArray) => {
+          setSolutionDog(dogArray);
+        })
+        .catch((err) => {
+          console.error('CLIENT ERROR: failed to start new round after correct answer', err);
+        });
+    }
+  };
+
+  useEffect(() => {
+    getDogs()
+      .then((dogArray) => setSolutionDog(dogArray));
+  }, []);
+
+  const dogButtons = dogs.map((url, index) => {
+    // const noDomain = url.slice(url.indexOf('breeds/') + 7);
+    // let breed = noDomain.slice(0, noDomain.indexOf('/'));
+    // if (breed.includes('-')) {
+    //   breed = breed.split('-').reverse().join(' ');
+    // }
+    const breed = parseUrl(url);
     return (
       <Button value={url} style={{ margin: '20px' }} key={index} onClick={handleAnswerSubmission} type="button">{breed}</Button>
     );

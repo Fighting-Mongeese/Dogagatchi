a690ca9a3a8ad94a5c9dd34d57ffe940def87577
jshep23prog <jshep23@gmail.com>
2023-11-06 20:01:36 -0600

    "final commit before deployment end of day 11/5"

--- server/index.js
+++ server/index.js
@@ -302,6 +302,9 @@ app.get('/leaderboard/:type', (req, res) => {
   }
 });
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'))
+})
 //GET request to '/search/:username' should query the database for the user and send back user data
 app.get('/searchUser/:username', (req, res) => {
   const { username } = req.params

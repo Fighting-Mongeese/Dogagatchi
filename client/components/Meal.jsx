b0ea1963f8ebd5143b338e5a849ceb6e7bfca77e
sydney andre <s.a.andre0217@gmail.com>
2023-11-07 15:15:29 -0600

    add/ resturant component created front end- no functionality yet

--- client/components/Meal.jsx
+++ client/components/Meal.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 function Meal(props){
-  const getCost = () => {
-    return Math.floor(Math.random() * 20)
-  }
+  
   return(
-    <div>
-      <img src={`${props.meal.strMealThumb}`}/>
-      <h3>{ `${props.meal.strMeal} for ${getCost()} tokens` }</h3>
-      <Button>buy for my pup!</Button>
-    </div>
+      <div className="meal-container">
+        <img id="meal-item"className="meal-image"src={`${props.meal.image}`}/>
+        <p id="meal-item">{props.meal.name}</p>
+        <p id="meal-item">{props.meal.cost}</p>
+        <Button id="meal-item" className="meal-button">buy for my pup!</Button>
+      </div>
   )
 }
 
--- client/components/Restaurant.jsx
+++ client/components/Restaurant.jsx
@@ -1,37 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
-import Categories from '../../server/Categories.js'
+import Meals from '../../server/Categories.js'
 import Meal from './Meal.jsx';
 
 function Restaurant(){
-  let buttonKey = 0
-  const [meals, setMeals] = useState([])
-  
-  function getMeals(category){
-    return axios(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
-    .then((mealsArr) => {
-      console.log('success', mealsArr.data.meals)
-      // console.log('param', category)
-      setMeals(mealsArr.data.meals)
-    })
-    .catch((err) => {
-      console.log('get meals error (client)', err);
-    })
-  }
+  //const [meals, setMeals] = useState([])
+
   return(
     <div>
       <h2>Bow Wow's Chow</h2>
-      <div>
-        {Categories.map((category) => (
-          <Button
-          key={buttonKey++}
-          onClick={() => {getMeals(category.strCategory)}}
-          >{category.strCategory}</Button>
-        ))}
-      </div>
-      <div>
-        {meals.map((mealObj) => (
+      <div className="meals-container">
+        {Meals.map((mealObj) => (
           <Meal 
           key={mealObj.idMeal}
           meal={mealObj}
@@ -42,3 +22,26 @@ function Restaurant(){
   )
 }
 export default Restaurant
+
+/**
+ * <div>
+        {Categories.map((category) => (
+          <Button
+          key={buttonKey++}
+          onClick={() => {getMeals(category.strCategory)}}
+          >{category.strCategory}</Button>
+        ))}
+      </div>
+
+       function getMeals(category){
+    return axios(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
+    .then((mealsArr) => {
+      console.log('success', mealsArr.data.meals)
+      // console.log('param', category)
+      setMeals(mealsArr.data.meals)
+    })
+    .catch((err) => {
+      console.log('get meals error (client)', err);
+    })
+  }
+ */

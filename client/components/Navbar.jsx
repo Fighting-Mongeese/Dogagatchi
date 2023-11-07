c2f429de5c36ad8f8563a1a027aae5a245dc53b7
evmaperry <evmaperry@gmail.com>
2023-11-07 17:11:37 -0500

    CLIENT/remove unneeded import statements from navbar

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Nav, Navbar, } from "react-bootstrap";
 import { useNavigate, Link } from 'react-router-dom';
-import axios from "axios";
 
 function NavBar(props) {
 

878b4eaef2c0889c6b83d43833a471d9b2c0ecfe
evmaperry <evmaperry@gmail.com>
2023-11-10 12:04:56 -0500

    CLIENT / cards on user page size properly

--- client/components/Kennel.jsx
+++ client/components/Kennel.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
 import axios from "axios";
 import Dog from "./Dog.jsx";
 
@@ -9,54 +10,48 @@ function Kennel(props) {
 
 
   return (
-    <div>
-      <div
-        style={{
-          height: 800,
-          overflow: "auto-hidden",
-        }}
-      >
-        <div>
-            {Array.isArray(dogs) && dogs.length > 0
-              ? dogs
-                  .filter((dog) => {
-                    const now = new Date().getTime();
-                    const feed =
-                      ((Date.parse(dog.feedDeadline) - now) / 86400000) * 100;
-                    const walk =
-                      ((Date.parse(dog.walkDeadline) - now) / 86400000) * 100;
+    <div style={{
+      height: 800,
+      overflow: "auto-hidden",
+    }}>
+      {Array.isArray(dogs) && dogs.length > 0
+        ? dogs
+          .filter((dog) => {
+            const now = new Date().getTime();
+            const feed =
+              ((Date.parse(dog.feedDeadline) - now) / 86400000) * 100;
+            const walk =
+              ((Date.parse(dog.walkDeadline) - now) / 86400000) * 100;
 
-                    if (walk < 0 || feed < 0) {
-                      alert(`${dog.name} ran away!`);
-                      axios
-                        .delete(`/dog/${dog._id}`)
-                        .then(getDogs)
-                        .catch((err) => {
-                          console.error(err);
-                        });
+            if (walk < 0 || feed < 0) {
+              alert(`${dog.name} ran away!`);
+              axios
+                .delete(`/dog/${dog._id}`)
+                .then(getDogs)
+                .catch((err) => {
+                  console.error(err);
+                });
 
-                      return false;
-                    } else {
-                      return true;
-                    }
-                  })
-                  .map((dog) => {
-                    return (
-                      <div
-                        className="item"
-                        key={dog._id}
-                      >
-                        <Dog
-                          dogObj={dog}
-                          dogs={dogs}
-                          // setDogs={setDogs}
-                        />
-                      </div>
-                    );
-                  })
-              : ""}
-        </div>
-      </div>
+              return false;
+            } else {
+              return true;
+            }
+          })
+          .map((dog) => {
+            return (
+              <div
+                className="item"
+                key={dog._id}
+              >
+                <Dog
+                  dogObj={dog}
+                  dogs={dogs}
+                // setDogs={setDogs}
+                />
+              </div>
+            );
+          })
+        : ""}
     </div>
   );
 }

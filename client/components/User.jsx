8102517a0473884505d287a6cff3f74700be190d
gfish94 <grmyfisher@gmail.com>
2023-11-11 09:41:02 -0600

    pass setCoins as props to make user component source of truth for coinCount, bugfix dog care logic

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import { ListGroup, Table, Card, Col, Container, Row } from "react-bootstrap";
-import NavBar from './Navbar.jsx';
-import DogShop from './DogShop.jsx'
-import Achievements from './Achievements.jsx';
-import Kennel from './Kennel.jsx';
-import Pantry from './Pantry.jsx'
-import axios from 'axios'
+import NavBar from "./Navbar.jsx";
+import DogShop from "./DogShop.jsx";
+import Achievements from "./Achievements.jsx";
+import Kennel from "./Kennel.jsx";
+import Pantry from "./Pantry.jsx";
+import axios from "axios";
 
 function User(props) {
   const [user, setUser] = useState("");
@@ -13,7 +13,7 @@ function User(props) {
   const [dogCount, setDogCount] = useState(0);
   const [ownDogs, setOwnDogs] = useState(0);
   const [coins, setCoins] = useState(0);
-  const [color, setColor] = useState("#ade3e3")
+  const [color, setColor] = useState("#ade3e3");
   const [correctQuestionCount, setCorrectQuestionCount] = useState(0);
   const [dogs, setDogs] = useState([]);
   const dogsRef = useRef(dogs);
@@ -30,25 +30,22 @@ function User(props) {
       })
       .catch((err) => console.error("getLeaders ERROR (client):", err));
 
-   setPage()
-    axios.get(`/dog/users/${userObj._id}`)
-      .then((dogArr) => {
-        setOwnDogs(dogArr.data.dogsArr.length)
-
-      })
+    setPage();
+    axios.get(`/dog/users/${userObj._id}`).then((dogArr) => {
+      setOwnDogs(dogArr.data.dogsArr.length);
+    });
 
     getKennel();
-  }, [coins])
+  }, [coins]);
 
   const setPage = () => {
-    axios.get(`/user/${userObj._id}`)
-      .then((user) => {
-        setUser(user.data[0])
-        setCorrectQuestionCount(user.data[0].questionCount)
-        setDogCount(user.data[0].dogCount)
-        setCoins(user.data[0].coinCount)
-      })
-  }
+    axios.get(`/user/${userObj._id}`).then((user) => {
+      setUser(user.data[0]);
+      setCorrectQuestionCount(user.data[0].questionCount);
+      setDogCount(user.data[0].dogCount);
+      setCoins(user.data[0].coinCount);
+    });
+  };
 
   const getKennel = () => {
     axios
@@ -61,73 +58,69 @@ function User(props) {
       });
   };
 
-
   return (
     <Container>
-
-      <Row  >
-        <Col style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }} >
-          <DogShop />
+      <Row>
+        <Col
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <DogShop setCoins={setCoins} />
         </Col>
       </Row>
-      <Row >
+      <Row>
         <Col xs={4}>
           <div className="user-stats">
-
-            {
-
-              globalRank === 1 ? (
-                <div>
-                  <h2 id="heady">🥇</h2>
-                 <h2 id="heady" className='shimmer'>{user.username}'s Kennel</h2>
-                </div>
-                 ) :
-            globalRank === 3 ?(
+            {globalRank === 1 ? (
+              <div>
+                <h2 id="heady">🥇</h2>
+                <h2
+                  id="heady"
+                  className="shimmer"
+                >
+                  {user.username}'s Kennel
+                </h2>
+              </div>
+            ) : globalRank === 3 ? (
               <div>
                 <h2 id="heady">🥉</h2>
-               <h2 id="heady" className='shimmer'>{user.username}'s Kennel</h2>
+                <h2
+                  id="heady"
+                  className="shimmer"
+                >
+                  {user.username}'s Kennel
+                </h2>
               </div>
-               ): globalRank === 2 ?(
-                <div>
-                  <h2 id="heady">🥈</h2>
-                 <h2 id="heady" className='shimmer'>{user.username}'s Kennel</h2>
-                </div>
-                 ): 
-                 (<h2>{user.username}'s Kennel</h2>)
-               }
-            <Card
-              style={{ backgroundColor: "#4c5f63" }}
-            >
-      <Card.Header
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          fontWeight: "bold",
-          fontSize: "large",
-          backgroundColor: color
-        }}
-      >
-        {{coins} > 1 ? <p>Coin: {coins}</p> : <p>Coins: {coins}</p>}
-      </Card.Header>
-      
-        <Card.Header
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          fontWeight: "bold",
-          fontSize: "large",
-          backgroundColor: color
-        }}
-      >
-        Global Rank: #{globalRank}
-      </Card.Header>
+            ) : globalRank === 2 ? (
+              <div>
+                <h2 id="heady">🥈</h2>
+                <h2
+                  id="heady"
+                  className="shimmer"
+                >
+                  {user.username}'s Kennel
+                </h2>
+              </div>
+            ) : (
+              <h2>{user.username}'s Kennel</h2>
+            )}
+            <Card style={{ backgroundColor: "#4c5f63" }}>
+              <Card.Header
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  fontWeight: "bold",
+                  fontSize: "large",
+                  backgroundColor: color,
+                }}
+              >
+                {{ coins } > 1 ? <p>Coin: {coins}</p> : <p>Coins: {coins}</p>}
+              </Card.Header>
 
               <Card.Header
                 style={{
@@ -136,7 +129,20 @@ function User(props) {
                   alignItems: "center",
                   fontWeight: "bold",
                   fontSize: "large",
-                  backgroundColor: color
+                  backgroundColor: color,
+                }}
+              >
+                Global Rank: #{globalRank}
+              </Card.Header>
+
+              <Card.Header
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  fontWeight: "bold",
+                  fontSize: "large",
+                  backgroundColor: color,
                 }}
               >
                 Correct Answers: {correctQuestionCount}
@@ -149,28 +155,26 @@ function User(props) {
                   alignItems: "center",
                   fontWeight: "bold",
                   fontSize: "large",
-                  backgroundColor: color
+                  backgroundColor: color,
                 }}
               >
                 Purchasable Dogs: {dogCount}
               </Card.Header>
 
-      <Card.Header
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          fontWeight: "bold",
-          fontSize: "large",
-          backgroundColor: color
-        }}
-      >
-        <p>Owned Dogs: {ownDogs}</p>
-      </Card.Header>
+              <Card.Header
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  fontWeight: "bold",
+                  fontSize: "large",
+                  backgroundColor: color,
+                }}
+              >
+                <p>Owned Dogs: {ownDogs}</p>
+              </Card.Header>
             </Card>
-
           </div>
-
 
           <Achievements
             user={user}
@@ -179,20 +183,20 @@ function User(props) {
         </Col>
 
         <Col xs={8}>
-
-          {
-            dogs.length !== 0 ? (
-        <div className="dogs">
-          <Kennel className="user-kennel" 
-          dogs={dogs}
-          getKennel={getKennel}
-          setCoins={setCoins}
-          />
-        </div>): (<h1>Start playing Pooch Picker to earn dogs to adopt!</h1>)
-       }
+          {dogs.length !== 0 ? (
+            <div className="dogs">
+              <Kennel
+                className="user-kennel"
+                dogs={dogs}
+                getKennel={getKennel}
+                setCoins={setCoins}
+              />
+            </div>
+          ) : (
+            <h1>Start playing Pooch Picker to earn dogs to adopt!</h1>
+          )}
         </Col>
       </Row>
-      
     </Container>
   );
 }

a4de353b112dfef43e50d47209583e7dd9ad9319
sydney andre <s.a.andre0217@gmail.com>
2023-11-11 11:28:35 -0600

    update/ meal cost

--- server/Categories.js
+++ server/Categories.js
@@ -2,35 +2,35 @@ module.exports = [
   {
     name: "Pawfect Banana Pancakes",
     image: "https://www.themealdb.com/images/media/meals/sywswr1511383814.jpg",
-    cost: 10,
+    cost: 6,
     fullTime: "placeholder",
     idMeal: 52855
   },
   {
     name: "Bark-becue Ribs",
     image: "https://www.seriouseats.com/thmb/y3PCzOFEIIvfVjFGNtpa0JOT_vc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2016__06__20150730-anova-sous-vide-rib-guide-food-lab-primary-wet-white-j-kenji-lopez-alt-ed91c537b77b4a559a78d5570e52b439.jpg",
-    cost: 10,
+    cost: 6,
     fullTime: "placeholder",
     idMeal: 52952
   },
   {
     name: "Silly Goose Spaghetti",
     image: "https://www.themealdb.com/images/media/meals/sutysw1468247559.jpg",
-    cost: 10,
+    cost: 6,
     fullTime: "placeholder",
     idMeal: 52796
   },
   {
     name: "Good Boy Burger",
     image: "https://www.themealdb.com/images/media/meals/urzj1d1587670726.jpg",
-    cost: 10,
+    cost: 6,
     fullTime: "placeholder",
     idMeal: 52819
   },
   {
     name: "Hot Diggity Donuts",
     image: "https://www.themealdb.com/images/media/meals/4i5cnx1587672171.jpg",
-    cost: 10,
+    cost: 6,
     fullTime: "placeholder",
     idMeal: 52905
   },
